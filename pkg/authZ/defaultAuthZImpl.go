abb2471bc64172079eb1e09061f10024dd41ed67
Doron Podoleanu <doronp@il.ibm.com>
2015-11-02 15:53:33 +0200

    Fix for golint
    Signed-off-by: Doron Podoleanu <doronp@il.ibm.com>

--- pkg/authZ/defaultAuthZImpl.go
+++ pkg/authZ/defaultAuthZImpl.go
@@ -14,7 +14,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//DefaultImp - Default basic label based implementation of ACLs & tenancy enforcment 
+//DefaultImp - Default basic label based implementation of ACLs & tenancy enforcment
 type DefaultImp struct{}
 
 //Init - Any required initialization
